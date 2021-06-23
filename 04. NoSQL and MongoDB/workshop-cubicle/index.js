0e5fc1c4642d802648d6e186ee3c6032f039daaa
mitetodb <65173088+mitetodb@users.noreply.github.com>
2021-06-23 22:11:42 +0200

    Create config and services

--- 04. NoSQL and MongoDB/workshop-cubicle/index.js
+++ 04. NoSQL and MongoDB/workshop-cubicle/index.js
@@ -1,20 +1,9 @@
-// initialize express app
-// setup handlebars
-// setup static files
-// setup storage middleware
-// set route handlebars (controller actions)
-
 const express = require('express');
-const hbs = require('express-handlebars');
 
-const { init: storage } = require('./models/storage');
+const expressConfig = require('./config/express');
+const routesConfig = require('./config/routes');
 
-const { catalog } = require('./controllers/catalog');
-const { about } = require('./controllers/about');
-const { details } = require('./controllers/details');
-const { create, post: createPost } = require('./controllers/create');
-const { edit, post: editPost } = require('./controllers/edit');
-const { notFound } = require('./controllers/notFound');
+const { init: storage } = require('./services/storage');
 
 start();
 
@@ -22,31 +11,12 @@ async function start() {
 
     const port = 3000;
     const app = express();
-    
 
-    app.engine('hbs', hbs({
-        extname: '.hbs'
-    }));
-    
-    app.set('view engine', 'hbs');
-    app.use('/', express.static('static'));
-    app.use('/js', express.static('js'));
-    app.use(express.urlencoded({ extended: false })); // body-parser as middleware
+    expressConfig(app);
     app.use(await storage()); // init from storage as middleware
-
+    routesConfig(app);
     
-    app.get('/', catalog);
-    app.get('/about', about);
-    app.get('/details/:id', details);
 
-    app.get('/create', create);
-    app.post('/create', createPost);
 
-    app.get('/edit/:id', edit);
-    app.post('/edit/:id', editPost);
-    
-    app.all('*', notFound);
-
-    
     app.listen(port, () => console.log(`Server is listening on port ${port} ...`));
 }
